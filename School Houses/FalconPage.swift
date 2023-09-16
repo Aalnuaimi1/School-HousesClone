9c633a8459caf4a8d37c95402432e7519ec12a6e
Aaesha Alnauimi <something@aaeshaalnuaimi.com>
2023-09-16 10:23:36 +0400

    Falcon page

---
+++ School Houses/FalconPage.swift
@@ -0,0 +1,86 @@
+//
+//  FalconPage.swift
+//  School Houses
+//
+//  Created by Aaesha Alnuaimi on 16/09/2023.
+//
+
+import SwiftUI
+
+struct FalconPage: View {
+    var body: some View {
+        
+        ZStack{
+            
+            Rectangle()
+                .foregroundColor(.blue)
+                .frame(width: 355, height: 360)
+                .cornerRadius(20)
+                .offset(y: -200)
+            
+            Color.orange.opacity(0.1)
+                .edgesIgnoringSafeArea(.all)
+            
+            Text("Choose from the")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.white)
+                .offset(y: -250)
+            
+            Text("following")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.white)
+                .offset(y: -200)
+            
+            Text("   Donate   ")
+                .font(.title)
+                .padding(30)
+                .background(.blue.opacity(0.8))
+                .cornerRadius(50)
+                .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 10)
+                .offset(y: 50)
+            
+            Text("  Recycle  ")
+                .font(.title)
+                .padding(30)
+                .background(.blue.opacity(0.8))
+                .cornerRadius(50)
+                .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 10)
+                .offset(y: 159)
+            
+            Text("  Receive  ")
+                .font(.title)
+                .padding(30)
+                .background(.blue.opacity(0.8))
+                .cornerRadius(50)
+                .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 10)
+                .offset(y: 265)
+            
+            Text("Account")
+                .font(.headline)
+                .foregroundColor(.gray)
+                .offset(x: -120, y: 370)
+            
+            Text("Homepage")
+                .font(.headline)
+                .foregroundColor(.gray)
+                .offset(x: -1, y: 370)
+            
+            Text("Houses")
+                .font(.headline)
+                .foregroundColor(.gray)
+                .offset(x: 120, y: 370)
+            
+        
+            
+            
+        }
+    }
+    
+    struct FalconPage_Previews: PreviewProvider {
+        static var previews: some View {
+            FalconPage()
+        }
+    }
+}
--- School Houses/SelectionPage.swift
+++ School Houses/SelectionPage.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 struct SelectionPage: View {
     var body: some View {
+        
+        
         ZStack{
+            Circle()
+                .frame(width: 90, height: 90)
+                .foregroundColor(Color.red.opacity(0.5))
+                .offset(x: -160, y: -80)
+            
+            Circle()
+                .frame(width: 90, height: 90)
+                .foregroundColor(Color.red.opacity(0.3))
+                .offset(x: -110, y: -110)
+            
+            Circle()
+                .frame(width: 90, height: 90)
+                .foregroundColor(Color.red.opacity(1))
+                .offset(x: -40, y: -35)
             Color.orange.opacity(0.1)
           .edgesIgnoringSafeArea(.all)
+            Text("Select your House")
+                .bold()
+                .font(.largeTitle)
+                .offset(y: -250)
+            
             Text("Select your House")
                 .bold()
                 .font(.largeTitle)
@@ -21,6 +42,52 @@ struct SelectionPage: View {
                 .bold()
                 .font(.largeTitle)
                 .offset(y: -210)
+            
+            
+            Text("   Stallions   ")
+                .bold()
+                .font(.title2)
+                .padding(25)
+                .foregroundColor(.black)
+                .background(Color.red)
+                .cornerRadius(20)
+                .shadow(color: .gray.opacity(0.3), radius: 10, x: 0, y: 10)
+                .padding()
+                .offset(x:-90,y:-50)
+            
+            Text("  Scorpions ")
+                .bold()
+                .font(.title2)
+                .padding(25)
+                .foregroundColor(.black)
+                .background(Color.yellow)
+                .cornerRadius(20)
+                .shadow(color: .gray.opacity(0.3), radius: 10, x: 0, y: 10)
+                .padding()
+                .offset(x:90,y:-50)
+            
+            Text("   Camels    ")
+                .bold()
+                .font(.title2)
+                .padding(25)
+                .foregroundColor(.black)
+                .background(Color.green)
+                .cornerRadius(20)
+                .shadow(color: .gray.opacity(0.3), radius: 10, x: 0, y: 10)
+                .padding()
+                .offset(x:-90,y:80)
+            
+            Text("    Falcons    ")
+                .bold()
+                .font(.title2)
+                .padding(25)
+                .foregroundColor(.black)
+                .background(Color.blue)
+                .cornerRadius(20)
+                .shadow(color: .gray.opacity(0.3), radius: 10, x: 0, y: 10)
+                .padding()
+                .offset(x:90,y:80)
+                
         
             Text("   Stallions   ")
                 .bold()
